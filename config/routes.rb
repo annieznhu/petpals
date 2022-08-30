1bd2bf4c29f418ffdc023dd944fbd61e941b5a2e
vincent brunel <vincent.brunel20@gmail.com>
2022-08-30 12:30:20 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :pages, only: %i[home home_user faq]
+  resources :profiles, only: %i[edit upgrade]
+  resources :dogs, only: %i[index show edit upgrade]
+  resources :likes, only: %i[create]
+  resources :matches, only: %i[index show]
+  resources :messages, only: %i[create]
+  resources :meetings, only: %i[index new create]
+  resources :walks, only: %i[create]
+  resources :places, only: %i[index new create]
+  resources :reviews, only: %i[new create]
 end
