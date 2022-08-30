170e02bd5520902750a8f40a277d0fc68c275caf
Annie HU <zhuoni1996@gmail.com>
2022-08-30 15:53:26 +0200

    model done, db done, routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,32 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticated :user do
+    root to: "pages#home_user", as: :user_root
+  end
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :pages, only: %i[home home_user faq]
-  resources :profiles, only: %i[edit update]
-  resources :dogs, only: %i[index show edit update] do 
+  resource :profile, only: %i[edit update]
+  resource :dog, only: %i[edit update]
+
+  resources :dogs, only: %i[index show] do
     resources :likes, only: %i[create]
   end
 
   resources :matches, only: %i[index show] do
     resources :messages, only: %i[create]
-    resources :meetings, only: %i[index new create]
+    resources :meetings, only: %i[new create]
   end
-  
+
+  resources :meetings, only: %i[index]
+
   resources :places, only: %i[index new create] do
     resources :walks, only: %i[create]
     resources :reviews, only: %i[new create]
   end
+
+  get 'faq', to: 'pages#faq'
 end
