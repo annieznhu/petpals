af65ad8f334471b8ddbdd413d0d263e6cf52ab4e
vincent brunel <vincent.brunel20@gmail.com>
2022-08-31 12:51:42 +0200

    seeds without cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_132919) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_31_095946) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_132919) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.string "name"
   end
 
   create_table "reviews", force: :cascade do |t|
