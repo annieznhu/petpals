1076797da34d30f6e4c6b251906f98526d8affce
Annie HU <zhuoni1996@gmail.com>
2022-09-06 16:27:51 +0200

    add autocomplete to address in simple form for of place

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import AddressAutocompleteController from "./address_autocomplete_controller"
+application.register("address-autocomplete", AddressAutocompleteController)
+
 import FlatpickrController from "./flatpickr_controller"
 application.register("flatpickr", FlatpickrController)
 
