c7683de55418727fbcc04dec592f1b31085f3862
Annie HU <zhuoni1996@gmail.com>
2022-09-06 10:50:40 +0200

    config heure local pour chatroom fixed &message activ logo fixed

--- config/application.rb
+++ config/application.rb
@@ -17,12 +17,13 @@ module Petpals
     config.load_defaults 7.0
 
     config.i18n.default_locale = :fr
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Paris"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
