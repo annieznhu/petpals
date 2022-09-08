6b865717fa092c9b33fce81dff620cbb95ed1cc2
Annie HU <zhuoni1996@gmail.com>
2022-09-08 12:46:27 +0200

    front chatroom done + fixe problem with word break

--- app/javascript/controllers/modal_blur_controller.js
+++ app/javascript/controllers/modal_blur_controller.js
@@ -2,14 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal-blur"
 export default class extends Controller {
-  static targets = ["map"]
+  static targets = ["map", "button"]
 
   blur() {
     this.mapTarget.classList.add("modal-blur")
+    this.buttonTarget.classList.add("modal-blur")
   }
 
   clear() {
     this.mapTarget.classList.remove("modal-blur")
+    this.buttonTarget.classList.remove("modal-blur")
+    console.log("hello")
   }
 
 }
